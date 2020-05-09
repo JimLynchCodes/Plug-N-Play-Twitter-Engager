b21f5b51b1a3b6accf3687fc28b652830f06cb66
JimLynchCodes <mrdotjim@gmail.com>
2020-05-09 15:06:23 -0400

    update bash file to take arbitrary flags and forward it to npm start

--- run-twitter-engager.sh
+++ run-twitter-engager.sh
@@ -13,5 +13,18 @@ cd ~/Git-Projects/Plug-N-Play-Twitter-Engager/
 # Use project's preferred node version from .nvmrc file.
 nvm use
 
-# Run the cron job!
-npm start
+# Run this bash script with arbitrary arguments, and it will call "npm start"
+# with those arguments. If no arguments passed in it just runs "npm start".
+npm_parameters=""
+
+if [ "$#" -eq "0" ]; then
+   npm start
+   exit
+fi
+
+for i in $@
+do 
+  npm_parameters="$npm_parameters $i"
+done
+
+eval npm start -- $npm_parameters
